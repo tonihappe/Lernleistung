2294b8791f5e0efdcd088839c2fb17e707b9c74c
tonihappe <46331585+tonihappe@users.noreply.github.com>
2019-01-06 21:30:19 +0100

    Zerlegung der Teilkräfte in X und Y --> Zerlegung FR noch NICHT

--- Besondere_Lernleistung_xy.py
+++ Besondere_Lernleistung_xy.py
@@ -1,15 +1,39 @@
 import math
 
-def getbeschl(PR):
+def getbeschl(PRx,PRy,PMx,PMy):
 	mM = 7.349e22 #in kg
 	mE = 5.972e24 #in kg
 	mR = 5000 #in kg
 	gamma = 6.67408e-11 #Gravitationskonstante m^3/s^2kg
-	rEM = 3844e5 #Abstand Erde-Mond
+	
+	#Abstände/Entfernungen
+	rEM = 3844e5 #Abstand Erde-Mond NOCH ZU ÄNDERN WENN SICH MOND BEWEGT
+	
+	rEX = PRx#-PEx da die Erde auf dem Ursprung liegt
+	rEY = PRy#-PEy da die Erde auf dem Ursprung liegt
+	rER = math.sqrt(rEX*rEX+rEY*rEY)
 
-	FE =-1*gamma*mR*mE/(PR**2) #negativ, da erde "hinter" raumschiff liegt auf x-Achse
-	FM = gamma*mR*mM/((rEM-PR)**2)
-	FRES = FE+FM
+	rMX = PRx-PMx
+	rMY = PRy-PMy
+	rMR = math.sqrt(rMx*rMx+rMy*rMy)
+
+	#Berechnungen der Kräfte der Erde/des Mondes
+	FER =-1*gamma*mR*mE/(rER**2) #negativ, da erde "hinter" raumschiff liegt auf x-Achse
+	FMR = gamma*mR*mM/((rMR-PR)**2)
+
+	#Zerlegung der Kräfte --> Bestimmung der notwendigen Winkel
+	betaREF=math.tan(rEX/rEY)
+	alphaMER= math.pi-betaREF
+	FEx = math.cos(alphaMER)*FER
+	FEy = math.sin(alphaMER)*FER
+
+	betaFRM = math.tan(rMX/rMY) #umbenennen?
+	alphaRME = math.pi-betaFRM
+	FMx = math.cos(alphaRME)*FMR
+	FMy = math.sin(alphaRME)*FMR
+	#############################################################################################################################################################################################
+
+	#alt FRES = FER+FMR
 	aR = FRES/mR
 	return aR,FRES
 
@@ -20,16 +44,24 @@ def getbeschl(PR):
 n= 1000000
 rM = 3476e3 #Radius Mond m
 rE = 6371e3 # Radius Erde m 
-rEM = 3844e5 #Abstand Erde-Mond
+rEM = 3844e5 #Abstand Erde-Mond NOCH ZU ÄNDERN WENN SICH MOND BEWEGT
 h = 1 #Schrittweite
 t=[0]*n
-PR=[0]*n
-v=[0]*n
-a=[0]*n
+PRx=[0]*n
+PRy=[0]*n
+PEx=[0]*n
+PEy=[0]*n
+PMx=[0]*n
+PMy=[0]*n
+vx=[0]*n
+vy=[0]*n
+ax=[0]*n
+ay=[0]*n
 
 #Anfangswerte
 t[0]=0
-PR[0]=6371e3	# Startpunkt des R (Radius der Erde)
+PRx[0]=6371e3	# Startpunkt des R (Radius der Erde)
+PRy[0]=0
 
 print("Startgeschwindigkeit: ")
 v[0]=int(input()) #Startgeschwindigkeit
@@ -41,7 +73,7 @@ ausg.write(str(t[0])+"\t"+str(PR[0])+"\t"+str(v[0])+"\t"+str(a[0])+"\n")
 
 #Schleife für einzelne Berechnung am jeweiligen Punkt
 for i in range(1,n,1):
-	aR, FRES =getbeschl(PR[i-1])
+	aR, FRES =getbeschl(PRx[i-1])
 	print("FRES = ", FRES,"\naR = ",aR)
 
 	#k berechnet x 
