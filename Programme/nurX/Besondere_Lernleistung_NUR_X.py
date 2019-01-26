f50ffa7a5781b1587e71a5e45d73ab70eb089112
tonihappe <46331585+tonihappe@users.noreply.github.com>
2019-01-26 01:17:30 +0100

    Struktogramme
    Struktogramme für UP und HP erstellt und in das Dokument eingefügt
    
    Programm übersichtlicher gemacht und alles mögliche beschriftet
    
    Neue Textdatei für alles was aussortiert wurde

--- Programme/nurX/Besondere_Lernleistung_NUR_X.py
+++ Programme/nurX/Besondere_Lernleistung_NUR_X.py
@@ -4,42 +4,42 @@ def getbeschl(PR):
 	mM = 7.349e22 #in kg
 	mE = 5.972e24 #in kg
 	mR = 5000 #in kg
-	G = 6.67408e-11 #Gravitationskonstante m^3/s^2kg
-	rEM = 3844e5 #Abstand Erde-Mond
+	G = 6.67408e-11 #in m^3/s^2kg; Gravitationskonstante
+	rEM = 3844e5 #in m; Abstand Erde-Mond
 
-	FE =-1*G*mR*mE/(PR**2) #negativ, da erde "hinter" raumschiff liegt auf x-Achse
-	FM = G*mR*mM/((rEM-PR)**2)
-	FRES = FE+FM
-	aR = FRES/mR
+	#Berechnung der Kräfte und der Beschleunigung
+	FE =-1*G*mR*mE/(PR**2) #negativ, da die Erde auf der x-Achse "hinter" dem Raumschiff liegt
+	FM = G*mR*mM/((rEM-PR)**2) 
+	FRES = FE+FM #resultierende Kraft auf das Raumschiff
+	aR = FRES/mR #resultierende Beschleunigung
+
 	return aR, FRES,FE,FM
 
 
-
-
 #deklarationen/initialisierungen
-n= 1000000
+n= 1000000 #Anzahl an maximalen Wiederholungen
+h = 1 #Schrittweite
+
+t=[0]*n #Zeit
+PR=[0]*n #Punt des Raumschiffes
+v=[0]*n #Geschwindigkeit des Raumschiffes
+
 rM = 3476e3 #Radius Mond m
 rE = 6371e3 # Radius Erde m 
 rEM = 3844e5 #Abstand Erde-Mond
-h = 1 #Schrittweite
-t=[0]*n
-PR=[0]*n
-v=[0]*n
-#a=[0]*n
-
 
 
 #Anfangswerte
 t[0]=0
-PR[0]=6371e3	# Startpunkt des R (Radius der Erde)
+PR[0]=6371e3	# Startpunkt des Raumschiffs (Radius der Erde)
 
 print("Startgeschwindigkeit: ")
-v[0]=int(input()) #Startgeschwindigkeit
+v[0]=int(input()) #Eingabe der Startgeschwindigkeit
 print("\n\n")
 
 aR, FRES,FE,FM = getbeschl(PR[0])
 ausg = open ("Ausgabe.dat","w")
-ausg.write(str(t[0])+"\t"+str(PR[0])+"\t"+str(v[0])+"\t"+str(aR)+"\t"+str(FRES)+"\t"+str(FE)+"\t"+str(FM)+"\n")
+ausg.write(str(t[0])+"\t"+str(PR[0])+"\t"+str(v[0])+"\t"+str(aR)+"\t"+str(FRES)+"\t"+str(FE)+"\t"+str(FM)+"\n") #Anfangswerte in die Datei schreiben
 
 #Schleife für einzelne Berechnung am jeweiligen Punkt
 for i in range(1,n,1):
@@ -50,6 +50,7 @@ for i in range(1,n,1):
 	#l berechnet v
 	#kräfte bei zwischenschritten als Unterprogramm
 	
+	#RungeKutta4
 	kx1 = v[i-1]
 	lx1 = aR
 		
@@ -67,7 +68,9 @@ for i in range(1,n,1):
 	v[i] = v[i-1]+ ((h/6) * (lx1 + 2 * lx2 + 2* lx3 + lx4))
 	aR, FRES,FE,FM = getbeschl(PR[i-1])
 	
+	
 	print("DURCHGANG NUMMER: ", i)
+	#Test ob Raumschiff Mond oder Erde erreicht hat
 	if (PR[i]<=rE):
 		print("Auf Erde aufgeschlagen!")
 		break
@@ -77,6 +80,6 @@ for i in range(1,n,1):
 
 	#werte in datei
 	ausg.write(str(t[0])+"\t"+str(PR[0])+"\t"+str(v[0])+"\t"+str(aR)+"\t"+str(FRES)+"\t"+str(FE)+"\t"+str(FM)+"\n")
-	#force.write(str(FRx[i-1])+"\t"+str(FRy[i-1])+"\n")
+	
 
 ausg.close()
