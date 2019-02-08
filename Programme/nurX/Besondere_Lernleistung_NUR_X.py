2e08a460c2c4f5023ad83c22a669406d11b66932
tonihappe <46331585+tonihappe@users.noreply.github.com>
2019-02-08 22:35:33 +0100

    Backup 190208
    RK4 Fertig mit Skizze
    Programm: Test ob lagrange punkt erreicht
    Abfrage ob gebremst werden soll

--- Programme/nurX/Besondere_Lernleistung_NUR_X.py
+++ Programme/nurX/Besondere_Lernleistung_NUR_X.py
@@ -11,7 +11,7 @@ def getbeschl(PR):
 	FE =-1*G*mR*mE/(PR**2) #negativ, da die Erde auf der x-Achse "hinter" dem Raumschiff liegt
 	FM = G*mR*mM/((rEM-PR)**2) 
 	FRES = FE+FM #resultierende Kraft auf das Raumschiff
-	aR = FRES/mR #resultierende Beschleunigung
+	aR = (FRES/mR) #resultierende Beschleunigung
 
 	return aR, FRES,FE,FM
 
@@ -27,6 +27,8 @@ v=[0]*n #Geschwindigkeit des Raumschiffes
 rM = 1737e3 #Radius Mond m
 rE = 6371e3 # Radius Erde m 
 rEM = 3844e5 #Abstand Erde-Mond
+
+#bremsantw=bool(False)
 
 
 #Anfangswerte
@@ -51,21 +53,22 @@ for i in range(1,n,1):
 	#kräfte bei zwischenschritten als Unterprogramm
 	
 	#RungeKutta4
-	kx1 = v[i-1]
-	lx1 = aR
-		
-	kx2 = v[i-1]+0.5*h*lx1
-	lx2 = aR
 	
-	kx3 = v[i-1]+0.5*h*lx2
-	lx3 = aR
+	k1 = v[i-1]
+	l1 = aR
+
+	k2 = v[i-1]+0.5*h*l1
+	l2 = aR
 	
-	kx4 = v[i-1]+ h * lx3
-	lx4 = aR
-	
+	k3 = v[i-1]+0.5*h*l2
+	l3 = aR
+
+	k4 = v[i-1]+ h * l3
+	l4 = aR
+
 	t[i] = t[i-1]+h
-	PR[i] = PR[i-1]+ h/6.0 * (kx1 + 2 * kx2 + 2* kx3 + kx4)
-	v[i] = v[i-1]+ ((h/6.0) * (lx1 + 2 * lx2 + 2* lx3 + lx4))
+	PR[i] = PR[i-1]+ h/6.0 * (k1 + 2 * k2 + 2* k3 + k4)
+	v[i]  = v[i-1] + h/6.0 * (l1 + 2 * l2 + 2* l3 + l4)
 	aR, FRES,FE,FM = getbeschl(PR[i-1])
 	
 	
@@ -74,15 +77,29 @@ for i in range(1,n,1):
 	if (PR[i]<=rE):
 		print("Auf Erde aufgeschlagen!")
 		break
+
 	elif(PR[i]>(rEM-rM)):
 		print("Auf Mond aufgeschlagen!")
 		break
+		
+	#if (math.fabs(FRES)<=0.1 and bremsantw==False):
+	#	print ("LAGRANGEPUNKT ERREICHT\n Soll der Bremsvorgang eingeleitet werden?\n\n[1] für Ja\n[2] für Nein\n")
+	#	bremsen=int(input())
+		
+	#	while (bremsen <= 0 or bremsen >= 3):#Falls ein ungültiger Wert eingegeben wurde wird diese Schleife ausgeführt
+	#		print("Bitte geben Sie [1] oder [2] ein!")
+	#		bremsen=int(input())
 
+	#	bremsantw=True # wird auf True gesetzt, damit nicht erneut gefragt wird, falls bereits geantwortet wurde
+
+	
+
+	#if (lagrinp==1):
+
+	
 	#werte in datei
 	ausg.write(str(t[i])+"\t"+str(PR[i])+"\t"+str(v[i])+"\t"+str(aR)+"\t"+str(FRES)+"\t"+str(FE)+"\t"+str(FM)+"\n")
 	
 
 ausg.close()
 
-#Bremsvorgang mit reinbringe#
-#if anweisung ob lagrange --> Zündung bremsdüsen
